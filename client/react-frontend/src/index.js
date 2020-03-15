aad93c56f111f36cf5ba874d394d1461847041ed
YanaBrewster <yana.brewster@gmail.com>
2020-03-16 10:37:33 +1300

    react project setup

---
+++ client/react-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+    <h1> Hello world </h1>
+    </div>
+  );
+}
+
+ReactDOM.rener(App,document.getElementById('root'));
+
+
+// class component
+class 
